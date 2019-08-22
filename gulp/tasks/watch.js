592daba50360454d0ea8eca63bd90ef1df897a46
lechestercat <chesterortega1991@gmail.com>
2019-08-22 15:09:04 -0400

    Complete header styles

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,13 +1,12 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch'),
-browserSync = require('browser-sync').create();
+  watch = require('gulp-watch'),
+  browserSync = require('browser-sync').create();
 
 gulp.task('watch', function() {
-
   browserSync.init({
     notify: false,
     server: {
-      baseDir: "app"
+      baseDir: 'app'
     }
   });
 
@@ -19,9 +18,15 @@ gulp.task('watch', function() {
     gulp.start('cssInject');
   });
 
+  watch('./app/assets/scripts/**/*.js', function() {
+    gulp.start('scriptsRefresh');
+  });
 });
 
 gulp.task('cssInject', ['styles'], function() {
-  return gulp.src('./app/temp/styles/styles.css')
-    .pipe(browserSync.stream());
+  return gulp.src('./app/temp/styles/styles.css').pipe(browserSync.stream());
+});
+
+gulp.task('scriptsRefresh', ['scripts'], function() {
+  browserSync.reload();
 });
