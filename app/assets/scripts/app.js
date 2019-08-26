8630c918bbfdcfd076a42882ae80edad4f48a7cf
lechestercat <chesterortega1991@gmail.com>
2019-08-26 15:55:22 -0400

    Complete reveal on scroll behavior

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,7 @@
 import MobileMenu from './modules/mobilemenu';
+import RevealOnScroll from './modules/reveal-on-scroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($('.feature-item'), '85%');
+new RevealOnScroll($('.testimonials'), '60%');
