aadaf5676977bed6d559e13ab92e91b3f15c930e
lechestercat <chesterortega1991@gmail.com>
2019-08-28 00:55:17 -0400

    Add sticky header

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,7 +1,9 @@
 import MobileMenu from './modules/mobilemenu';
 import RevealOnScroll from './modules/reveal-on-scroll';
 import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($('.feature-item'), '85%');
 new RevealOnScroll($('.testimonials'), '60%');
+var stickyHeader = new StickyHeader();
