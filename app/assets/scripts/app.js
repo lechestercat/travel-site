11f0def685f0858accfcaf375ab1b467fd8b8e45
lechestercat <chesterortega1991@gmail.com>
2019-08-28 18:48:06 -0400

    Complete modal overlay

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -2,8 +2,10 @@ import MobileMenu from './modules/mobilemenu';
 import RevealOnScroll from './modules/reveal-on-scroll';
 import $ from 'jquery';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($('.feature-item'), '85%');
 new RevealOnScroll($('.testimonials'), '60%');
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
