07d37f56eebc439163d9455733ebe66f7fa7d6f7
lechestercat <chesterortega1991@gmail.com>
2019-08-29 14:56:35 -0400

    Add browser support

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,16 @@
-var path = require('path');
-
 module.exports = {
   entry: {
-    App: './app/assets/scripts/app.js',
-    Vendor: './app/assets/scripts/Vendor.js'
+    App: "./app/assets/scripts/App.js",
+    Vendor: "./app/assets/scripts/Vendor.js"
   },
   output: {
-    path: path.resolve(__dirname, './app/temp/scripts'),
-    filename: '[name].js'
+    path: "./app/temp/scripts",
+    filename: "[name].js"
   },
   module: {
     loaders: [
       {
-        loader: 'babel-loader',
+        loader: 'babel',
         query: {
           presets: ['es2015']
         },
@@ -21,4 +19,4 @@ module.exports = {
       }
     ]
   }
-};
+}
